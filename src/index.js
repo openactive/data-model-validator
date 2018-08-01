eb35bb90b84fbef6d204927f568b7740cfff9acd
Pete Walker <pete.walker@me.com>
2018-08-01 12:06:27 +0100

    Added defaultRules to the package exports

--- src/index.js
+++ src/index.js
@@ -2,10 +2,12 @@
  * data-model-validator
  * MIT Licensed
  */
+const defaultRules = require('./rules');
 const validate = require('./validate');
 
 function createValidator() {
   const root = {
+    defaultRules,
     validate,
   };
   return root;
