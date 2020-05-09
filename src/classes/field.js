22dbc562a7bf8ab3485f46cbfd5e106d1c57a27c
nickevansuk <2616208+nickevansuk@users.noreply.github.com>
2020-05-09 12:20:47 +0100

    Improved URL regex to include localhost

--- src/classes/field.js
+++ src/classes/field.js
@@ -339,8 +339,40 @@ const Field = class {
   }
 };
 
-// Source: https://gist.github.com/dperini/729294
-Field.URL_REGEX = /^(?:(?:https?|ftp):\/\/)(?:\S+(?::\S*)?@)?(?:(?!(?:10|127)(?:\.\d{1,3}){3})(?!(?:169\.254|192\.168)(?:\.\d{1,3}){2})(?!172\.(?:1[6-9]|2\d|3[0-1])(?:\.\d{1,3}){2})(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[1-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)(?:\.(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)*(?:\.(?:[a-z\u00a1-\uffff]{2,}))\.?)(?::\d{2,5})?(?:[/?#]\S*)?$/i;
+// Source: adapted from https://gist.github.com/dperini/729294
+Field.URL_REGEX = new RegExp(
+  '^'
+    // protocol identifier (mandatory)
+    // short syntax // not permitted
+    + '(?:(?:https?):\\/\\/)'
+    + '(?:'
+      // IP address dotted notation octets
+      // excludes loopback network 0.0.0.0
+      // excludes reserved space >= 224.0.0.0
+      // excludes network & broadcast addresses
+      // (first & last IP address of each class)
+      + '(?:[1-9]\\d?|1\\d\\d|2[01]\\d|22[0-3])'
+      + '(?:\\.(?:1?\\d{1,2}|2[0-4]\\d|25[0-5])){2}'
+      + '(?:\\.(?:[1-9]\\d?|1\\d\\d|2[0-4]\\d|25[0-4]))'
+    + '|'
+      // host & domain names, may end with dot
+      // may not contain dots (e.g localhost)
+      + '(?:'
+        + '(?:'
+          + '[a-z0-9\\u00a1-\\uffff]'
+          + '[a-z0-9\\u00a1-\\uffff_-]{0,62}'
+        + ')?'
+        + '[a-z0-9\\u00a1-\\uffff]\\.?'
+      + ')+'
+      // TLD identifier name, may end with dot
+      + '(?:[a-z\\u00a1-\\uffff]{2,}\\.?)'
+    + ')'
+    // port number (optional)
+    + '(?::\\d{2,5})?'
+    // resource path (optional)
+    + '(?:[/?#]\\S*)?'
+  + '$', 'i',
+);
 
 Field.canBeTypeOfMapping = {
   'https://schema.org/Date': 'https://schema.org/Text',
