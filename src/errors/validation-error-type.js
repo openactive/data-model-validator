9fe086a58e78076a179ff864c0791ddd3e474bea
Timo Hill <tim.hill@theodi.org>
2020-11-05 10:19:50 +0000

    Adding rule to ensure repeatFrequency is accompanied by appropriate byDay/byWeek/byMonthDay values

--- src/errors/validation-error-type.js
+++ src/errors/validation-error-type.js
@@ -40,6 +40,7 @@ const ValidationErrorType = {
   TYPE_LIMITS_USE: 'type_limits_use',
   WRONG_BASE_TYPE: 'wrong_base_type',
   FIELD_NOT_ALLOWED: 'field_not_allowed',
+  REPEATFREQUENCY_MISALIGNED: 'repeatfrequency_misaligned',
 };
 
 module.exports = Object.freeze(ValidationErrorType);
