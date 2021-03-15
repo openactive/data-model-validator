95475755ad2784153b7c7da3194af9b2021d7be1
nickevansuk <2616208+nickevansuk@users.noreply.github.com>
2021-03-15 18:25:37 +0000

    Add test and bump version

--- src/helpers/property-spec.js
+++ src/helpers/property-spec.js
@@ -14,5 +14,9 @@ describe('PropertyHelper', () => {
       const result = PropertyHelper.isUrlTemplate('{description}{something-else}.');
       expect(result).toBe(false);
     });
+    it('should return false for URL that has previously caused issues', async () => {
+      const result = PropertyHelper.isUrlTemplate('https://reachstorageaccount.blob.core.windows.net/images/9142d700-adb7-4f7b-af10-fef045ff11f4/783cbdc4-c0c0-449b-8ce0-1e147337a628/The Regal - Bball Court.jpg');
+      expect(result).toBe(false);
+    });
   });
 });
