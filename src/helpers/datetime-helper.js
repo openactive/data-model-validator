f8678300cf3659952aa7f3e3f5d604ad8d9087bb
Jared Parnell <jared.parnell@opendataservices.coop>
2021-04-05 16:27:41 +0000

    helpers: Ensure node uses UTC envvar for datetime functions

--- src/helpers/datetime-helper.js
+++ src/helpers/datetime-helper.js
@@ -1,6 +1,9 @@
 const { DateTime } = require('luxon');
 
 function getDateTime(ianaTimezone, dateString, timeString) {
+  if (process.env.TZ !== 'UTC') {
+    throw new Error(`Schedule generation logic relies on 'TZ' env var being set to 'UTC'. It is currently: ${process.env.TZ}`);
+  }
   if (typeof dateString !== 'undefined' && typeof timeString !== 'undefined') {
     return DateTime.fromISO(`${dateString}T${timeString}`, { zone: ianaTimezone }).toJSDate();
   }
