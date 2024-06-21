openactive/data-model-validator

commit 0ffa3a4070e1c10bfdeef00d39632841f41c7f50

fix: Allow empty RPDE page (#444)

src/helpers/raw.js
@@ -1,31 +1,25 @@
+const _ = require('lodash');
+
 const RawHelper = class {
   static isRpdeFeed(data) {
-    if (
-      typeof data !== 'object'
-      || data === null
-      || data instanceof Array
-    ) {
+    if (!_.isPlainObject(data)) {
       return false;
     }
-    if (
-      typeof data.type === 'undefined'
-      && typeof data['@type'] === 'undefined'
-      && typeof data.items !== 'undefined'
-      && data.items instanceof Array
-    ) {
-      for (const item of data.items) {
-        if (
-          typeof item.state === 'string'
-          && (
-            item.state === 'updated'
-            || item.state === 'deleted'
-          )
-        ) {
-          return true;
-        }
+    const type = data['@type'] ?? data.type;
+    // This is a JSON-LD object with a @type
+    if (!_.isNil(type)) {
+      return false;
+    }
+    if (!Array.isArray(data.items)) {
+      return false;
+    }
+    for (const item of data.items) {
+      if (item.state !== 'updated' && item.state !== 'deleted') {
+        return false;
       }
     }
-    return false;
+    // If the page has no items (e.g. a last page), it's still considered an RPDE feed.
+    return true;
   }
 };
 

src/rules/raw/rpde-feed-rule-spec.js
@@ -44,6 +44,20 @@ describe('RpdeFeedRule', () => {
     expect(errors[0].severity).toBe(ValidationErrorSeverity.NOTICE);
   });
 
+  it('should return a notice for an empty RPDE feed', async () => {
+    const data = {
+      items: [],
+      next: 'https://example.org/api/feed/?afterId=ABCDEF09001015&afterTimestamp=1533206202992&limit=500',
+      license: 'https://creativecommons.org/licenses/by/4.0/',
+    };
+
+    const { errors } = await rule.validate(data);
+    expect(errors.length).toBe(1);
+
+    expect(errors[0].type).toBe(ValidationErrorType.FOUND_RPDE_FEED);
+    expect(errors[0].severity).toBe(ValidationErrorSeverity.NOTICE);
+  });
+
   it('should return a notice for an RPDE feed, and modify the data with a limit to the number of items', async () => {
     const feed = {
       items: [

commit c4e239619cd4cc0a4c917d3ff74fc46d3548c811

fix: Ensure compatibility with Babel (fix bug introduced in #444)

src/helpers/raw.js
@@ -5,7 +5,7 @@ const RawHelper = class {
     if (!_.isPlainObject(data)) {
       return false;
     }
-    const type = data['@type'] ?? data.type;
+    const type = data['@type'] || data.type;
     // This is a JSON-LD object with a @type
     if (!_.isNil(type)) {
       return false;